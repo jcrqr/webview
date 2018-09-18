4c74837f2e7a77ddd3130b65fc2d97c4c8e3f7ec
João Cerqueira <und0ck3d@protonmail.com>
2018-09-18 15:04:26 +0100

    Added comments to existing functions

--- src/webview.c
+++ src/webview.c
@@ -3,13 +3,17 @@
 #define WEBVIEW_IMPLEMENTATION
 #include "webview.h"
 
+// state_t
+// A structure to hold the webview state.
 typedef struct
 {
-    struct webview*            w;
+    struct webview*     w;
     ERL_NIF_TERM        atom_ok;
     ERL_NIF_TERM        atom_error;
 } state_t;
 
+// load
+// Loads the NIF.
 static int
 load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
 {
@@ -24,6 +28,8 @@ load(ErlNifEnv* env, void** priv, ERL_NIF_TERM load_info)
     return 0;
 }
 
+// unload
+// Unloads the NIF.
 static void
 unload(ErlNifEnv* env, void* priv)
 {
@@ -32,11 +38,16 @@ unload(ErlNifEnv* env, void* priv)
     enif_free(state);
 }
 
+// create
+// Creates a new webview.
+// Implements the `webview_init` function.
 static ERL_NIF_TERM
 create(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     state_t* state = (state_t*) enif_priv_data(env);
     struct webview *w = (struct webview *) calloc(1, sizeof(*w));
+
+    // get arguments
     ErlNifBinary title, url;
     int resizable, debug, width, height;
 
@@ -64,6 +75,7 @@ create(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
         return enif_make_badarg(env);
     }
 
+    // setup webview
     w->title = title.data;
     w->url = url.data;
     w->width = width;
@@ -71,9 +83,11 @@ create(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     w->resizable = resizable;
     w->debug = debug;
 
+    // release unecessary binaries
     enif_release_binary(&title);
     enif_release_binary(&url);
 
+    // init the webview
     if (webview_init(w) != 0) {
         enif_free(w);
 
@@ -85,38 +99,57 @@ create(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     return state->atom_ok;
 }
 
+// loop
+// Calls the webview loop. Accepts a `blocking` argument
+// that should be either `0` or `1` for non-blocking and blocking loop, respectively.
+// Implements the `webview_loop` function.
 static ERL_NIF_TERM
 loop(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     state_t* state = (state_t*) enif_priv_data(env);
+
+    // parse argument
     int blocking;
 
     if (enif_get_int(env, argv[0], &blocking) == 0) {
         return enif_make_badarg(env);
     }
 
+    // loop
     webview_loop(state->w, blocking);
 
     return state->atom_ok;
 }
 
+// set_title
+// Sets the webview title. Accepts a `title` argument that must be an Erlang binary.
+// Implements the `webview_loop` function.
+// TODO: this function should return the result of `webview_loop`.
 static ERL_NIF_TERM
 set_title(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     state_t* state = (state_t*) enif_priv_data(env);
+
+    // parse argument
     ErlNifBinary title_bin;
 
     if (enif_inspect_binary(env, argv[0], &title_bin) == 0) {
         return enif_make_badarg(env);
     }
 
+    // set the title
     webview_set_title(state->w, title_bin.data);
 
+    // release the title binary
     enif_release_binary(&title_bin);
 
     return state->atom_ok;
 }
 
+// set_fullscreen
+// Toggles the webview fullscreen property. Accepts a `fullscreen` argument
+// that should be either `0` or `1` to turn fullscreen off and on respectively.
+// Implements the `webview_set_fullscreen` function.
 static ERL_NIF_TERM
 set_fullscreen(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
@@ -132,23 +165,37 @@ set_fullscreen(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     return state->atom_ok;
 }
 
+// eval
+// Evaluates JavaScript code in the webview. Accepts a `js` argument that
+// should be an Erlang binary with the code to be evaluated.
+// Implements `webview_eval` function.
+// TODO: this function should return the result of `webview_eval`.
 static ERL_NIF_TERM
 eval(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     state_t* state = (state_t*) enif_priv_data(env);
+
+    // parse argument
     ErlNifBinary js_bin;
 
     if (enif_inspect_binary(env, argv[0], &js_bin) == 0) {
         return enif_make_badarg(env);
     }
 
+    // evaluate JS
     webview_eval(state->w, js_bin.data);
 
+    // release the binary
     enif_release_binary(&js_bin);
 
     return state->atom_ok;
 }
 
+// inject_css
+// Injects CSS code in the webview. Accepts a `css` argument that should be an
+// Erlang binary with the styles to be injected.
+// Implements `webview_inject_css` function.
+// TODO: this function should return the result of `webview_inject_css`.
 static ERL_NIF_TERM
 inject_css(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
@@ -166,6 +213,7 @@ inject_css(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
     return state->atom_ok;
 }
 
+// Initialize the NIF
 static ErlNifFunc nif_funcs[] = {
     {"create", 6, create},
     {"loop", 1, loop},
